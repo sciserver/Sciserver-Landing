1fc643a6a1d3bd20ab4da067c0088a388322fc83
Juliana Jaime <mj.jaime10@uniandes.edu.co>
2023-01-24 14:52:25 -0500

    Update environment.ts

--- config/environment.ts
+++ config/environment.ts
@@ -1,13 +1,8 @@
 // Map env variables and set defaults using the || operator in case no env variable is found
 
-let loginPortalUrl = 'https://kubetest.sciserver.org/julianas-sciserver/login-portal/';
-let graphqlUrl = 'https://kubetest.sciserver.org/julianas-sciserver/graphql';
-let baseUrl = 'http://localhost:3000';
-
-// if (process.env.ADYTON_ENV === 'prod') {
-// baseUrl = 'http://kubetest.sciserver.org';
-//   graphqlHttp = 'https://kubetest.sciserver.org/julianas-sciserver/graphql';
-// }
+let loginPortalUrl = '';
+let graphqlUrl = '';
+let baseUrl = '';
 
 export const env = {
   GRAPHQL_BASE_URL: graphqlUrl,
